907fd04cc240e84d931f9f05c1b25564840848a4
sladeykins-glitch <sladeykins@gmail.com>
2026-08-29 11:15:06 +0100

    Add tracker configuration placeholder

---
+++ config.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and replace the values after creating your Supabase project.
+window.AESTRA_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  campaignId: "00000000-0000-0000-0000-000000000001"
+};
