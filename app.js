05610fd44e56774b7d6e845fa2c6545aafd6850c
sladeykins-glitch <sladeykins@gmail.com>
2026-08-29 14:01:35 +0100

    Enable Aestra combat visual layout

--- app.js
+++ app.js
@@ -11,3 +11,4 @@ await import('./enhancements.js?v=2');
 await import('./status-fix.js?v=1');
 await import('./core-library.js?v=3');
 await import('./rules-aware.js?v=2');
+await import('./aestra-visuals.js?v=1');
