e3fb3bec91685e5225586117cc3eafe3e4e3f495
sladeykins-glitch <sladeykins@gmail.com>
2026-08-30 14:16:09 +0100

    Build 123: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aestra-v122';
-const SHELL=['./','./index.html','./styles.css?v=5','./aestra-icon.svg','./manifest.webmanifest','./version.json'];
+const CACHE='aestra-v123';
+const SHELL=['./','./index.html','./styles.css?v=5','./aestra-icon.svg','./manifest.webmanifest','./version.json','./hero-console-v2.js?v=1'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('aestra-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 function sameOrigin(req){try{return new URL(req.url).origin===self.location.origin}catch{return false}}
