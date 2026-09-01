332d66248f3389b9780e8a43adfb79a4d1fa18dd
sladeykins-glitch <sladeykins@gmail.com>
2026-09-01 12:09:35 +0100

    Refresh completion layer for Build library guard

--- techno-bootstrap.js
+++ techno-bootstrap.js
@@ -8,4 +8,4 @@ await load('./mutant-tools.js?v=1');
 await load('./pilot-tools.js?v=1');
 await load('./techno-fantasy-picker.js?v=2');
 await load('./techno-heroic-picker.js?v=1');
-await load('./completion-sweep.js?v=17');
+await load('./completion-sweep.js?v=18');
